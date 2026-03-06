b1173a8b2cd95c1aac1959fd48eecb102128ef39
alvescamila87 <alvescamila87@gmail.com>
2026-03-05 21:33:16 -0300

    feat props

--- class02/src/App.tsx
+++ class02/src/App.tsx
@@ -1,10 +1,42 @@
 //componente: função que retorna jsx
-function Button() {
-  return <button>My button</button>;
+
+// mais legível
+type ButtonProps = {
+  //backgroundColor: string;
+  backgroundColor: "#ff0000" | "#0000ff" | "#ffff00";
+  fontSize: number;
+  pillShape: boolean;
+};
+
+//function Button({ backgroundColor }: { backgroundColor: string }) {
+function Button({ backgroundColor, fontSize, pillShape }: ButtonProps) {
+  return (
+    <button
+      style={{
+        backgroundColor,
+        fontSize,
+        borderRadius: pillShape ? "99x" : "0x",
+      }}
+    >
+      My button
+    </button>
+  );
 }
 
 function App() {
-  return <Button />;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: "250px",
+        gap: "16px",
+      }}
+    >
+      <Button pillShape={true} fontSize={30} backgroundColor="#0000ff" />
+      <Button pillShape={true} fontSize={30} backgroundColor="#ffff00" />
+    </div>
+  );
 }
 
 export default App;
